c557e09fe7c0166ae20619180b4cdb10f8b85776
Brodino <119953295+Brodino96@users.noreply.github.com>
2023-10-10 10:42:59 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,37 @@
+-- Starts playing a sound locally on a single client.
+RegisterNetEvent("brodoPlayer:PlayOnOne")
+AddEventHandler("brodoPlayer:PlayOnOne", function(soundFile, soundVolume)
+    SendNUIMessage({
+        transactionType     = "playSound",
+        transactionFile     = soundFile,
+        transactionVolume   = soundVolume
+    })
+end)
+
+
+-- Starts playing a sound on all clients who are online in the server.
+RegisterNetEvent("brodoPlayer:Sound:PlayOnAll")
+AddEventHandler("brodoPlayer:PlayOnAll", function(soundFile, soundVolume)
+    SendNUIMessage({
+        transactionType     = "playSound",
+        transactionFile     = soundFile,
+        transactionVolume   = soundVolume
+    })
+end)
+
+
+-- Starts playing a sound on a client if the client is within the specificed maxDistance from the playOnEntity.
+-- @TODO Change sound volume based on the distance the player is away from the playOnEntity.
+RegisterNetEvent("brodoPlayer:PlayWithinDistance")
+AddEventHandler("brodoPlayer:PlayWithinDistance", function(playerNetId, maxDistance, soundFile, soundVolume)
+    local lCoords = GetEntityCoords(GetPlayerPed(-1))
+    local eCoords = GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(playerNetId)))
+    local distIs  = Vdist(lCoords.x, lCoords.y, lCoords.z, eCoords.x, eCoords.y, eCoords.z)
+    if(distIs <= maxDistance) then
+        SendNUIMessage({
+            transactionType     = "playSound",
+            transactionFile     = soundFile,
+            transactionVolume   = soundVolume
+        })
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+author "Brodino"
+description "Audioplayer based on plunkettscott's interact-sound"
+version "1.0"
+
+fx_version "cerulean"
+game "gta5"
+
+client_script "client.lua"
+
+ui_page("html/index.html")
+
+files({
+    "html/index.html",
+    "html/sounds/*"
+})
